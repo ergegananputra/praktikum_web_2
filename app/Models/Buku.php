1cb8309b04c5b81e3696e9367dbe2e9d0d1e54d8
ergegananputra <ergegananputra@gmail.com>
2023-11-14 22:39:51 +0700

    Add One-to-Many and Many-to-One relations between Buku and Gallery models

--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Buku extends Model
 {
@@ -16,7 +17,23 @@ class Buku extends Model
         'penulis',
         'harga',
         'tgl_terbit',
+        'filename', 
+        'filepath', 
         'created_at',
         'update_at'
     ];
+
+
+    /*
+    * Relasi One-to-Many
+    * ==================
+    * Buat function bernama galleries(), dimana model 'Buku' memiliki
+    * relasi One-to-Many (hasMany) terhadap model 'Gallery' yang
+    * memiliki foreign key 'buku_id'
+    */
+    public function galleries(): HasMany
+    {
+        return $this->hasMany(Gallery::class);
+    }
+
 }
